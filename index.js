abfd836e29589a5c15fa59e5670d8ffc41b37254
ilya <ilya.soloveyv@gmail.com>
2020-01-21 03:40:09 +0300

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  // console.log(msg)
+  console.log(msg)
   bot.sendMessage(chatId, 'Received your message');
 });
 
